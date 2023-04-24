5ee8abef9fe2716305114db1cc3b76aea0462034
elneven <elise.neven@hotmail.be>
2023-04-24 16:09:06 +0300

    Update 24/04 16:09

--- Post_processing/Codes/Matrix.m
+++ Post_processing/Codes/Matrix.m
@@ -15,11 +15,12 @@ t_steady.Properties.VariableNames = {'t1', 't2', 'P1', 'T1', 'P2', 'T2', 'P4', '
 %% Matrix of the mean values (one per experimental points)
 
 % Convertir la matrice en une table
-t_mean = table(Timestep_point(1,:).', Timestep_point(2,:).', P1_mean_points, P2_mean_points, Nexp_mean_points, W_mec_mean_points, X_mean_points, eta_v_mean_points, eta_is_r_mean_points, rp_mean_points);
+t_mean_sensor69 = table(Timestep_point(1,:).', Timestep_point(2,:).', P1_mean_points, P2_mean_points, Nexp_mean_points, Npump_mean_points, W_mec_mean_points, X_mean_points, eta_v_mean_points, eta_is_r_mean_points, rp_mean_points);
 
 %Nommer les colonnes de la table
-t_mean.Properties.VariableNames = {'t1', 't2', 'P1','P2', 'Nexp', 'W_mec', 'X', 'eta_v', 'eta_is_r', 'rp'}; 
+t_mean_sensor_69.Properties.VariableNames = {'t1', 't2', 'P1','P2', 'Nexp', 'Npump', 'W_mec', 'X', 'eta_v', 'eta_is_r', 'rp'}; 
+
 
 % Afficher la table
-% writetable(t_mean, 'Post_processing_firstexp.xlsx', 'Sheet', 4);
+writetable(t_mean_sensor69, 'C:\Users\elise\OneDrive\Documents\MATLAB\Thesis\Post_processing_TFE\Post_processing\Post Processing Results/Post_processing_sensor69.xlsx', 'Sheet', 1);
 
